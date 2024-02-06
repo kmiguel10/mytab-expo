d0b5dcc97d9b67339fe446c7994b34b1a359382d
Kent Miguel <kentmiguel10@gmail.com>
2024-02-06 14:43:38 -0500

    Update profile.tsx

--- app/(homepage)/profile.tsx
+++ app/(homepage)/profile.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from 'tamagui'
+import { Button, StyleSheet, View } from "react-native";
+import { Text } from "tamagui";
+
+import { Link } from "expo-router";
 
 export default function TabTwoScreen() {
   return (
-    <View flex={1} alignItems='center'>
-      <Text fontSize={20}>Tab Two</Text>
+    <View style={styles.container}>
+      <Text>Tab Two</Text>
+      {/* <EditScreenInfo path="app/(tabs)/two.tsx" /> */}
+      <Text>This will show all profile info and log out button</Text>
+      <Link href={"/"} replace asChild>
+        <Button title="Return to Home / Logout" />
+      </Link>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
