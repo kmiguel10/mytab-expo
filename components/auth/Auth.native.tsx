13eae738054095704e341139befae15f5cf112a0
Kent Miguel <kentmiguel10@gmail.com>
2024-04-22 16:09:54 -0400

    Attempt to implement Sign in with Apple

--- components/auth/Auth.native.tsx
+++ components/auth/Auth.native.tsx
@@ -1,6 +1,23 @@
 import { Platform } from "react-native";
 import * as AppleAuthentication from "expo-apple-authentication";
-import { supabase } from "@/lib/supabase";
+//import { supabase } from "@/lib/supabase";
+
+import "react-native-url-polyfill/auto";
+import { createClient } from "@supabase/supabase-js";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const supabase = createClient(
+  "http://127.0.0.1:54321",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+  {
+    auth: {
+      storage: AsyncStorage,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+    },
+  }
+);
 
 export function Auth() {
   if (Platform.OS === "ios")
@@ -18,6 +35,8 @@ export function Auth() {
                 AppleAuthentication.AppleAuthenticationScope.EMAIL,
               ],
             });
+
+            console.log("credentials: ", credential);
             // Sign in via Supabase Auth.
             if (credential.identityToken) {
               const {
@@ -28,6 +47,7 @@ export function Auth() {
                 token: credential.identityToken,
               });
               console.log(JSON.stringify({ error, user }, null, 2));
+              console.log("ERRORR, ", error);
               if (!error) {
                 // User is signed in.
               }
@@ -39,6 +59,7 @@ export function Auth() {
               // handle that the user canceled the sign-in flow
             } else {
               // handle other errors
+              console.log("Errors", e);
             }
           }
         }}
--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,13 +2,13 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "http://127.0.0.1:54321";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
-
-// const supabaseUrl = "https://xstshtvdjvmzebjctwmn.supabase.co";
+// const supabaseUrl = "http://127.0.0.1:54321";
 // const supabaseAnonKey =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdHNodHZkanZtemViamN0d21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNzg3MjgsImV4cCI6MjAyMjg1NDcyOH0.TZ5cmTcEW_1NKson2Wk6ZTq8mQTkCcZazam7IyZL9HI";
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
+
+const supabaseUrl = "https://xstshtvdjvmzebjctwmn.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdHNodHZkanZtemViamN0d21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNzg3MjgsImV4cCI6MjAyMjg1NDcyOH0.TZ5cmTcEW_1NKson2Wk6ZTq8mQTkCcZazam7IyZL9HI";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
