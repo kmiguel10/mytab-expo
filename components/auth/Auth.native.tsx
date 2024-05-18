319dae7fad16e59574b13190b9902ba9921b344b
Kent Miguel <kentmiguel10@gmail.com>
2024-05-18 16:01:40 -0400

    Connected to local db

--- components/auth/Auth.native.tsx
+++ components/auth/Auth.native.tsx
@@ -1,32 +1,35 @@
-import { Platform } from "react-native";
+import { Platform, useWindowDimensions } from "react-native";
 import * as AppleAuthentication from "expo-apple-authentication";
-//import { supabase } from "@/lib/supabase";
+import { supabase } from "@/lib/supabase";
 
 import "react-native-url-polyfill/auto";
-import { createClient } from "@supabase/supabase-js";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const supabase = createClient(
-  "http://127.0.0.1:54321",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
-  {
-    auth: {
-      storage: AsyncStorage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  }
-);
+// const supabase = createClient(
+//   "https://xstshtvdjvmzebjctwmn.supabase.co",
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzdHNodHZkanZtemViamN0d21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyNzg3MjgsImV4cCI6MjAyMjg1NDcyOH0.TZ5cmTcEW_1NKson2Wk6ZTq8mQTkCcZazam7IyZL9HI",
+//   {
+//     auth: {
+//       storage: AsyncStorage,
+//       autoRefreshToken: true,
+//       persistSession: true,
+//       detectSessionInUrl: false,
+//     },
+//   }
+// );
 
 export function Auth() {
+  const { width, height } = useWindowDimensions();
+
+  const appleButtonWidth = width * 0.76;
+  const appleButtonHeight = height * 0.045;
+
   if (Platform.OS === "ios")
     return (
       <AppleAuthentication.AppleAuthenticationButton
         buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
         buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
         cornerRadius={5}
-        style={{ width: 200, height: 64 }}
+        style={{ width: appleButtonWidth, height: appleButtonHeight }}
         onPress={async () => {
           try {
             const credential = await AppleAuthentication.signInAsync({
@@ -54,7 +57,7 @@ export function Auth() {
             } else {
               throw new Error("No identityToken.");
             }
-          } catch (e) {
+          } catch (e: any) {
             if (e.code === "ERR_REQUEST_CANCELED") {
               // handle that the user canceled the sign-in flow
             } else {
