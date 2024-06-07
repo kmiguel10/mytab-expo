1728af207045e97b383efd8e74ca232a65a023e0
Kent Miguel <kentmiguel10@gmail.com>
2024-06-06 23:50:42 -0400

    Added Toasts
    - Saving edit bill info toast
    - Extend duration toast

---
+++ components/bill-settings/extend-duration-toast.tsx
@@ -0,0 +1,47 @@
+import { formatDate, convertToLocalDate } from "@/lib/helpers";
+import { BillInfo } from "@/types/global";
+import { Toast } from "@tamagui/toast";
+
+interface ExtendDurationToastProps {
+  setOpenExtendDuration: React.Dispatch<React.SetStateAction<boolean>>;
+  openExtendDuration: boolean;
+  extendErrorMessage: string;
+  bill: BillInfo[];
+}
+
+const ExtendDurationToast: React.FC<ExtendDurationToastProps> = ({
+  setOpenExtendDuration,
+  openExtendDuration,
+  extendErrorMessage,
+  bill,
+}) => {
+  const errorTitle = "Error extending duration";
+  const successTitle = `Bill Duration Extended for ${bill[0]?.name}`;
+
+  const successMsg = ` New Duration ${formatDate(
+    convertToLocalDate(bill[0]?.start_date.toString())
+  )} - ${formatDate(convertToLocalDate(bill[0]?.end_date.toString()))}`;
+  return (
+    <Toast
+      onOpenChange={setOpenExtendDuration}
+      open={openExtendDuration}
+      animation="100ms"
+      enterStyle={{ x: -20, opacity: 0 }}
+      exitStyle={{ x: -20, opacity: 0 }}
+      opacity={1}
+      x={0}
+      backgroundColor={extendErrorMessage ? "$red8Light" : "$green8Light"}
+      width={"80%"}
+      justifyContent="center"
+    >
+      <Toast.Title textAlign="left">
+        {extendErrorMessage ? errorTitle : successTitle}
+      </Toast.Title>
+      <Toast.Description>
+        {extendErrorMessage ? extendErrorMessage : successMsg}
+      </Toast.Description>
+    </Toast>
+  );
+};
+
+export default ExtendDurationToast;
