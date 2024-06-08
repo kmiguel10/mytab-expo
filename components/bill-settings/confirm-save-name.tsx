09d73c91a6b35fe4fd211fac378a79d831835eee
Kent Miguel <kentmiguel10@gmail.com>
2024-06-07 21:44:00 -0400

    Changed message and added disable conditions

--- components/bill-settings/confirm-save-name.tsx
+++ components/bill-settings/confirm-save-name.tsx
@@ -1,4 +1,3 @@
-import { getMonthAndDateFromISOString } from "@/lib/helpers";
 import { supabase } from "@/lib/supabase";
 import { AlertCircle } from "@tamagui/lucide-icons";
 import React from "react";
@@ -13,57 +12,35 @@ import {
 } from "tamagui";
 import { StyledButton } from "../button/button";
 
-interface Member {
-  memberid: string;
-  userid: string;
-  isMemberIncluded: boolean;
-}
 interface Props {
-  name: string;
   billId: number;
   userId: string;
   disabled: boolean;
-  date: Date;
-  endDate: Date;
+  newBillName: string;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   setSaveNameError: React.Dispatch<React.SetStateAction<boolean>>;
-  setInitialDate: (date: string) => void;
-  newInitialDate: string;
-  newExpirationDate: string;
+  setInitialName: (newInitialName: string) => void;
 }
 //pass: name, billId, userId
 
 export const ConfirmSaveName: React.FC<Props> = ({
-  name,
   billId,
-  userId,
   disabled,
+  newBillName,
   setOpen,
   setSaveNameError,
-  setInitialDate,
-  date,
-  endDate,
-  newInitialDate,
-  newExpirationDate,
+  setInitialName,
 }) => {
   /************ States and Variables ************/
   const { width, height } = useWindowDimensions();
-  const confirmSaveMessage = `Are you sure you want to save changes to bill: \n"${name}"`;
-
-  const changeDateToTodayMsg =
-    "You are activating the bill today. This action cannot be undone. ";
-
-  const newExpirationDateMsg = `New Expiration Date: ${newExpirationDate}`;
-  const today = new Date();
-  const todayMonthDate = getMonthAndDateFromISOString(today.toISOString());
-  const dateMonthDate = getMonthAndDateFromISOString(date.toISOString());
+  const confirmSaveMessage = `Are you sure you want to change Bill Name to: \n"${newBillName}"`;
 
   /************ Functions ************/
   const onSubmit = async () => {
-    if (name) {
+    if (newBillName) {
       const { data, error } = await supabase
         .from("bills")
-        .update({ name: name, start_date: date, end_date: endDate })
+        .update({ name: newBillName })
         .eq("billid", billId)
         .select();
 
@@ -71,11 +48,7 @@ export const ConfirmSaveName: React.FC<Props> = ({
         console.log("submitted bill: ", data);
         setOpen(true);
         setSaveNameError(false);
-        setInitialDate(data[0].start_date);
-        // router.replace({
-        //   pathname: `/(bill)/mybill/${billId}`,
-        //   params: { userId: userId.toString() },
-        // });
+        setInitialName(newBillName);
       } else if (error) {
         console.log("ERROR", error);
         setSaveNameError(true);
@@ -127,20 +100,15 @@ export const ConfirmSaveName: React.FC<Props> = ({
             <AlertDialog.Title>Save changes</AlertDialog.Title>
             <AlertDialog.Description>
               <YStack gap="$2">
-                <Paragraph>{confirmSaveMessage}</Paragraph>
-                {todayMonthDate === dateMonthDate && (
-                  <Card backgroundColor={"$yellow7Light"} padding="$2">
-                    <XStack alignItems="center" gap="$2">
-                      <AlertCircle />
-                      <H4>Warning</H4>
-                    </XStack>
-                    <Paragraph padding="$1">{changeDateToTodayMsg}</Paragraph>
-                    <Paragraph padding="$1">{newExpirationDateMsg}</Paragraph>
-                  </Card>
-                )}
+                <Card backgroundColor={"$yellow7Light"} padding="$2">
+                  <XStack alignItems="center" gap="$2">
+                    <AlertCircle />
+                    <H4>Warning</H4>
+                  </XStack>
+                  <Paragraph padding="$1">{confirmSaveMessage}</Paragraph>
+                </Card>
               </YStack>
             </AlertDialog.Description>
-
             <XStack gap="$3" justifyContent="flex-end">
               <AlertDialog.Cancel asChild>
                 <StyledButton>Cancel</StyledButton>
